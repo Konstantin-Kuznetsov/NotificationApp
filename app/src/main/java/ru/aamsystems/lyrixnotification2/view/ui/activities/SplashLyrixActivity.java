0b19f7fe6a6c4d46a3671975df592740dea53fd0
k.kuznetsov <k.kuznetsov@aamsystems.ru>
2018-08-14 09:48:07 +0300

    добавлен splash screen

---
+++ app/src/main/java/ru/aamsystems/lyrixnotification2/view/ui/activities/SplashLyrixActivity.java
@@ -0,0 +1,19 @@
+package ru.aamsystems.lyrixnotification2.view.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by k.kuznetsov on 14.08.2018
+ */
+public class SplashLyrixActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
